11d93192379c9c9c6af35df6287544e7e7ca77d1
Hazmi Alfarizqi <contact@hzmi.xyz>
2025-02-13 09:46:54 +0700

    feat: add database schemas

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,36 +1,44 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
-import {
-  index,
-  integer,
-  pgTableCreator,
-  timestamp,
-  varchar,
-} from "drizzle-orm/pg-core";
+import { date, decimal, integer, pgEnum, pgTable, text, varchar } from "drizzle-orm/pg-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
-export const createTable = pgTableCreator((name) => `kasir-ujikom_${name}`);
+export const userLevel = pgEnum("user_levels", ["administrator", "user"]);
 
-export const posts = createTable(
-  "post",
-  {
-    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
-    ),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
-);
+export const users = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  username: varchar({ length: 50 }).notNull(),
+  password: text().notNull(),
+  level: userLevel().notNull()
+});
+
+export const products = pgTable("products", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  price: decimal({ precision: 10, scale: 2 }).notNull(),
+  stock: integer().notNull()
+});
+
+export const customers = pgTable("customers", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  address: text().notNull(),
+  phoneNumber: varchar({ length: 15 }).notNull()
+});
+
+export const sales = pgTable("sales", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  date: date().notNull(),
+  totalPrice: decimal({ precision: 10, scale: 2 }),
+  customerId: integer().references(() => customers.id, { onDelete: "restrict", onUpdate: "cascade" }),
+  userId: integer().references(() => users.id, { onDelete: "restrict", onUpdate: "cascade" })
+});
+
+export const salesDetails = pgTable("sales_details", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  salesId: integer().references(() => sales.id, { onDelete: "cascade", onUpdate: "cascade" }),
+  productId: integer().references(() => products.id, { onDelete: "restrict", onUpdate: "cascade" }),
+  amount: integer().notNull(),
+  subTotal: decimal({ precision: 10, scale: 2 }).notNull()
+});
