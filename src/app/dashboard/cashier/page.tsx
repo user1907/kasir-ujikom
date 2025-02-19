6f3057d9f14f17a7d3a0d79c7bc54937c2657204
Hazmi Alfarizqi <contact@hzmi.xyz>
2025-02-20 01:13:33 +0700

    feat(cashier): cart stuff

--- src/app/dashboard/cashier/page.tsx
+++ src/app/dashboard/cashier/page.tsx
@@ -2,14 +2,15 @@
 
 import { useBreadcrumb } from "@/components/providers/breadcrumb";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Table, TableBody, TableCell, TableHeader, TableRow } from "@/components/ui/table";
 import { formatCurrency } from "@/lib/numberFormat";
 import { api } from "@/trpc/react";
 import { type ColumnDef } from "@tanstack/react-table";
-import { Search } from "lucide-react";
+import { MinusIcon, PlusIcon, Search } from "lucide-react";
 import { useEffect, useMemo, useState } from "react";
 
 export default function CashierPage() {
@@ -31,6 +32,7 @@ export default function CashierPage() {
   }, [products.data, nameFilter]);
   type Product = typeof filteredProducts[0];
   const [productsInCart, setProductsInCart] = useState<{ product: Product, quantity: number }[]>([]);
+  const [paymentAmount, setPaymentAmount] = useState<number>(0);
 
   const addToCart = (product: Product) => {
     setProductsInCart((prevCart) => {
@@ -44,6 +46,21 @@ export default function CashierPage() {
         });
       }
       return [...prevCart, { product, quantity: 1 }];
+    });
+  };
+
+  const removeFromCart = (product: Product) => {
+    setProductsInCart((prevCart) => {
+      const existingProduct = prevCart.find(item => item.product.id === product.id);
+      if (existingProduct && existingProduct.quantity > 1) {
+        return prevCart.map((item) => {
+          if (item.product.id === product.id) {
+            return { ...item, quantity: item.quantity - 1 };
+          }
+          return item;
+        });
+      }
+      return prevCart.filter(item => item.product.id !== product.id);
     });
   };
 
@@ -71,12 +88,8 @@ export default function CashierPage() {
     <div
       key={index}
       className="
-        p-4 border rounded-lg shadow-sm
-        min-h-[20vh] w-full
-        flex flex-col justify-between
-        hover:bg-accent hover:text-accent-foreground active:bg-accent active:text-accent-foreground focus-visible:ring-2 transition duration-200
+        p-4 border rounded-lg shadow-sm min-h-[20vh] flex flex-col justify-between
       "
-      onClick={() => addToCart(product)}
     >
       <div className="flex flex-col flex-grow">
         <div className="flex items-center gap-2 mb-2">
@@ -90,38 +103,27 @@ export default function CashierPage() {
           {" "}
           {product.stock}
         </span>
+        <Button className="ml-auto py-1 px-2" onClick={() => addToCart(product)}>Tambahkan</Button>
       </div>
     </div>
   );
 
-  const columns: ColumnDef<Product>[] = [
-    {
-      header: "Produk",
-      accessorKey: "name"
-    },
-    {
-      header: "Harga",
-      accessorKey: "price",
-      cell: ({ row }) => formatCurrency(row.original.price)
-    },
-    {
-      header: "Jumlah"
-    },
-    {
-      header: "Subtotal",
-      cell: ({ row }) => formatCurrency(row.original.price)
-    }
-  ];
+  const totalAmount = productsInCart.reduce(
+    (acc, item) => acc + Number(item.product.price) * item.quantity,
+    0
+  );
+
+  const changeAmount = Math.max(paymentAmount - totalAmount, 0);
 
   return (
-    <Card className="flex flex-col h-full">
+    <Card className="flex flex-col h-full" id="main-content">
       <CardHeader>
         <CardTitle>Kasir</CardTitle>
         <CardDescription>Layani pelanggan</CardDescription>
       </CardHeader>
       <CardContent className="flex flex-col h-full">
-        <div className="flex flex-row gap-4 h-full">
-          <Card className="w-3/4 h-full">
+        <div className="flex flex-row gap-2 h-full">
+          <Card className="w-3/4 h-full" id="product-list">
             <CardHeader>
               <CardTitle>Katalog Produk</CardTitle>
             </CardHeader>
@@ -143,20 +145,28 @@ export default function CashierPage() {
               </div>
             </CardContent>
           </Card>
-          <Card className="h-full flex-grow">
+          <Card className="flex-grow flex flex-col h-full" id="cart">
             <CardHeader>
               <CardTitle className="flex items-center">
                 <span>Keranjang</span>
-                <Button className="ml-auto" onClick={() => setProductsInCart([])}>Kosongkan keranjang</Button>
+                <Button
+                  className="ml-auto"
+                  onClick={() => {
+                    setProductsInCart([]);
+                    setPaymentAmount(0);
+                  }}
+                >
+                  Kosongkan keranjang
+                </Button>
               </CardTitle>
             </CardHeader>
-            <CardContent className="overflow-auto">
-              <Table className="w-full">
+            <CardContent className="flex-grow overflow-auto">
+              <Table className="h-full">
                 <TableHeader>
                   <TableRow>
                     <TableCell>Produk</TableCell>
                     <TableCell>Harga</TableCell>
-                    <TableCell>Jumlah</TableCell>
+                    <TableCell className="text-center">Jumlah</TableCell>
                     <TableCell>Subtotal</TableCell>
                   </TableRow>
                 </TableHeader>
@@ -165,17 +175,67 @@ export default function CashierPage() {
                     <TableRow key={index}>
                       <TableCell>{item.product.name}</TableCell>
                       <TableCell>{formatCurrency(item.product.price)}</TableCell>
-                      <TableCell>{item.quantity}</TableCell>
+                      <TableCell className="text-center">
+                        <div className="flex items-center justify-center">
+                          <Button
+                            variant="ghost"
+                            onClick={() => removeFromCart(item.product)}
+                          >
+                            <MinusIcon />
+                          </Button>
+                          <span className="mx-2">{item.quantity}</span>
+                          <Button
+                            variant="ghost"
+                            onClick={() => addToCart(item.product)}
+                          >
+                            <PlusIcon />
+                          </Button>
+                        </div>
+                      </TableCell>
                       <TableCell>{formatCurrency((Number(item.product.price) * item.quantity).toString())}</TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
               </Table>
-              {/* <div className="mt-4">
-                <span className="font-semibold">Total: </span>
-                <span>{formatCurrency(productsInCart.reduce((total, item) => total + item.product.price * item.quantity, 0))}</span>
-              </div> */}
             </CardContent>
+            <CardFooter className="w-full mt-auto">
+              <div className="flex flex-col w-full">
+                <div className="flex justify-between w-full">
+                  <span>Total:</span>
+                  <span className="font-semibold">
+                    {formatCurrency(totalAmount.toString())}
+                  </span>
+                </div>
+                <div className="flex justify-between w-full">
+                  <span>Pembayaran:</span>
+                  <span className="font-semibold">
+                    {formatCurrency(paymentAmount.toString())}
+                  </span>
+                </div>
+                {changeAmount > 0 && (
+                  <div className="flex justify-between w-full">
+                    <span>Kembalian:</span>
+                    <span className="font-semibold">
+                      {formatCurrency(changeAmount.toString())}
+                    </span>
+                  </div>
+                )}
+                <div className="flex space-x-4 w-full mt-3">
+                  <Input
+                    placeholder="Masukkan pembayaran"
+                    className="flex-grow"
+                    type="number"
+                    onChange={e => setPaymentAmount(Number(e.target.value))}
+                  />
+                  <Button
+                    className="flex-shrink-0"
+                    disabled={paymentAmount < totalAmount}
+                  >
+                    Bayar
+                  </Button>
+                </div>
+              </div>
+            </CardFooter>
           </Card>
         </div>
       </CardContent>
--- src/lib/numberFormat.ts
+++ src/lib/numberFormat.ts
@@ -1,8 +1,4 @@
 export const formatCurrency = (value: string) => {
-  if (value === "0") {
-    return "Gratis";
-  }
-
   return new Intl.NumberFormat("id-ID", {
     style: "currency",
     currency: "IDR",
